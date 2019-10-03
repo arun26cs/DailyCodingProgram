4fd6eb1e77f590a03c9d6e5d0c2c615549cf0f0c
arun26cs <arun26cs@gmail.com>
2019-10-03 14:13:49 -0400

    commit-prob140-linear time and constant space

--- Prob140.java
+++ Prob140.java
@@ -1,8 +1,36 @@
 package DCP;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Prob140 {
 
-	public static void main() {
-		
+	public static void main(String args[]) {
+		Stack<Integer> st = new Stack<Integer>();
+		Scanner sc = new Scanner(System.in);
+		int n=Integer.parseInt(sc.nextLine());
+		LinkedList<Integer> input = new LinkedList<Integer>();
+		for(int i=0;i<n;i++) {
+			int inp = Integer.parseInt(sc.nextLine());
+			input.add(inp);
+		}
+		Collections.sort(input);
+		System.out.println(input);
+		int i=0;
+		int prev=0;
+		while(i<input.size()) {
+			if(input.get(i)!=prev) {
+				st.add(input.get(i));
+			}else {
+				st.pop();
+			}
+			System.out.println(st);
+			prev=input.get(i);
+			i++;
+		}
+		System.out.println(st);
 	}
 }
